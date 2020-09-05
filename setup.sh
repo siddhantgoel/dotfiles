f3c0d4985413e34f98b8f2cd511c7a8ed7159f9c
Siddhant Goel <me@sgoel.org>
2020-09-05 21:43:07 +0200

    chore: replace nvim with vim in setup.sh

--- setup.sh
+++ setup.sh
@@ -8,10 +8,10 @@ if ! [ -x "$(command -v stow)" ]; then
 fi
 
 stow tmux --target $HOME
+stow vim --target $HOME
 stow zsh --target $HOME
 
 stow crestic --target $HOME/.config/crestic
-stow nvim --target $HOME/.config/nvim
 stow systemd --target $HOME/.config/systemd
 
 echo "All done!"
