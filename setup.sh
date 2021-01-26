d47358d5f34d249cce7fdf2640613e5554b134ad
Siddhant Goel <me@sgoel.dev>
2021-01-26 20:30:31 +0100

    glrnvim: add config

--- setup.sh
+++ setup.sh
@@ -8,9 +8,9 @@ if ! [ -x "$(command -v stow)" ]; then
 fi
 
 stow tmux --target $HOME
-stow vim --target $HOME
 stow zsh --target $HOME
 
+stow glrnvim --target $HOME/.config
 stow crestic --target $HOME/.config/crestic
 stow nvim --target $HOME/.config/nvim
 stow systemd --target $HOME/.config/systemd
