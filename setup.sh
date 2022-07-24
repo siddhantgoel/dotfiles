8f704110e3ba4b02aba0ee3d568e3151efd1fe31
Siddhant Goel <me@sgoel.dev>
2022-07-24 14:23:27 +0200

    crestic: encrypted password file

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
-set -e
+set -xe
+
+if ! [ -x "$(command -v age)" ]; then
+  echo "Please install age." >&2
+  exit 1
+fi
 
 if ! [ -x "$(command -v dotbot)" ]; then
   echo "Please install dotbot." >&2
   exit 1
 fi
 
+# decrypt crestic password
+age --decrypt --identity ~/.age/keys.txt --output crestic/password crestic/password.enc
+
+# install
 dotbot --config-file install.conf.yaml
